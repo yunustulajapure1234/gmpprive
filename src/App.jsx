1c693dbe01c58887bd50112d851fbf122abf6e6d
Yunus Kasim Tulajapure <yunustulajapure524@gmail.com>
2026-02-18 16:45:16 +0530

    Fix admin dashoard remove protected

--- src/App.jsx
+++ src/App.jsx
@@ -55,9 +55,9 @@ function App() {
                 <Route
                   path="/admin/dashboard"
                   element={
-                    <ProtectedRoute>
+                    
                       <AdminDashboard />
-                    </ProtectedRoute>
+                    
                   }
                 />
 
